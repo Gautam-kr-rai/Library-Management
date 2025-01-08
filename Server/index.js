49f2bd1ef39337f0f84b8812db9f538f8c68ba39
Gautam Kumar <g.k965069@gmail.com>
2025-01-08 14:32:37 +0530

    fixes

--- Server/index.js
+++ Server/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const path = require("path")
+const path = require("path");
+const fs = require("fs");
 const app = express();
 
-//import middlewares
+// Import middlewares
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
 const cors = require("cors");
 
-// const __dirname= path.resolve()
-//middlewares
+// Middlewares
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
@@ -26,7 +26,7 @@ app.use(
   })
 );
 
-//routes
+// Routes
 const authRoutes = require("./routes/user");
 const bookRoutes = require("./routes/book");
 const transactionRoutes = require("./routes/Transaction");
@@ -34,32 +34,29 @@ const transactionRoutes = require("./routes/Transaction");
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/book", bookRoutes);
 app.use("/api/v1/transaction", transactionRoutes);
-//connect to db
+
+// Connect to DB
 const connectDB = require("./config/db");
 connectDB();
 
-//cloudinary
+// Cloudinary
 const { cloudinaryConnect } = require("./config/cloudinary");
 cloudinaryConnect();
-//server
-const PORT = process.env.PORT || 3000;
 
+// Serve static files (client/dist)
+const distPath = path.resolve(__dirname, "client", "dist");
 
-// if(process.env.NODE_ENV==="production"){
-//   app.use(express.static(path.join(__dirname,"/client/dist")))
-
-//   app.get("*", (req, res) => {
-//    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
-//  });
-// }
-
-if (process.env.NODE_ENV === "development") {
-  app.use(express.static(path.resolve(__dirname, "client", "dist")));
-
+if (fs.existsSync(distPath)) {
+  app.use(express.static(distPath));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+    res.sendFile(path.resolve(distPath, "index.html"));
   });
+} else {
+  console.error("Static files not found. Did you forget to build the client?");
 }
+
+// Server
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
